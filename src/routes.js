65e0b19fdf31ebfcc04501e6b44fafecaca01292
Corpella <alessandrocordella97@gmail.com>
2023-01-07 21:30:47 +0100

    feat: add new route with channel param, keep old one for retrocompatibility

--- src/routes.js
+++ src/routes.js
@@ -1,30 +1,50 @@
 const express = require("express")
+const Message = require("./models/message")
 
 const router = express.Router()
 
-const Message = require("./models/message")
-
-
-// Get chat history
-
+// TODO: remove once app is updated
 router.get("/:interval", async (req, res) => {
+    const days = typeof parseInt(req.params.interval) == 'number' ? req.params.interval : 1
+
+    const interval = new Date().getDate() - days
+
+    const fromDate = new Date().setDate(interval)
+
     try {
-        // const chatHistory = await Message.find()
-
-        const days = typeof parseInt(req.params.interval) == 'number' ? req.params.interval : 1
-
         const chatHistory = await Message.find({
             "createdAt": {
-                $gt: new Date(new Date().setDate(new Date().getDate() - days)),
+                $gt: fromDate,
                 $lt: new Date(),
-            }
+            },
         })
 
         res.json(chatHistory)
 
     } catch (error) {
         res.status(500).json({ message: error.message })
+    }
+})
 
+router.get("/:channel/:interval", async (req, res) => {
+    const days = typeof parseInt(req.params.interval) == 'number' ? req.params.interval : 1
+
+    const channel = req.params.channel
+    try {
+        const interval = new Date().getDate() - days
+        const fromDate = new Date().setDate(interval)
+        const chatHistory = await Message.find({
+            "createdAt": {
+                $gt: fromDate,
+                $lt: new Date(),
+            },
+            channel
+        })
+
+        res.json(chatHistory)
+
+    } catch (error) {
+        res.status(500).json({ message: error.message })
     }
 })
 
